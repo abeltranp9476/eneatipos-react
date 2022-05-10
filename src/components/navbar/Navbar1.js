05f4bc5d38a17a4d84fc61542f5d039f29429a26
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-05-10 17:15:51 -0400

    feat: create component Navbar1

---
+++ src/components/navbar/Navbar1.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Navbar, NavbarBrand, NavbarText, NavbarToggler, UncontrolledDropdown, DropdownMenu, NavItem, Nav, NavLink, DropdownToggle, Collapse, DropdownItem } from 'reactstrap';
+
+function Navbar1(props) {
+    return (
+        <div>
+            <Navbar
+                color="dark"
+                dark
+                expand="md"
+                fixed="top"
+                light
+            >
+                <NavbarBrand href="/">
+                    {props.title}
+                </NavbarBrand>
+                <NavbarToggler onClick={function noRefCheck() { }} />
+                <Collapse navbar>
+                    <Nav
+                        className="me-auto"
+                        navbar
+                    >
+                        <NavItem>
+                            <NavLink href="/components/">
+                                Components
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">
+                                GitHub
+                            </NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown
+                            inNavbar
+                            nav
+                        >
+                            <DropdownToggle
+                                caret
+                                nav
+                            >
+                                Options
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Option 1
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Option 2
+                                </DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>
+                                    Reset
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                    <NavbarText>
+                        Simple Text
+                    </NavbarText>
+                </Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Navbar1
